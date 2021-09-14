0499b2f74a30a479267b7a4f4be7f51a8e751e7e
kalyani joshi <kalyani121@gmail.com>
2021-09-14 17:18:59 -0400

    initial user routes

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const path = require('path');
 const express = require('express');
-const mysql = require('mysql2');
-const dotEnv = require('dotenv').config()
-const routes = require('./controllers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,4 +37,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
