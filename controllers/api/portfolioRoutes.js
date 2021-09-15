2c8f1b21d8e6f176797899f220a976c082d8ae24
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-09-15 15:34:39 -0400

    saving

--- controllers/api/portfolioRoutes.js
+++ controllers/api/portfolioRoutes.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const { Portfolio, Tickers, Investment } = require("../../models");
-=======
 const sequelize = require('../../config/connection');
 const { Portfolio, User, Investment, Tickers } = require("../../models");
->>>>>>> 4599e36f390db606f1e297fb97c7df4159633f28
 
 router.post("/", async (req, res) => {
     try {
@@ -26,36 +22,6 @@ router.post("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-<<<<<<< HEAD
-    const portfolioData = await Portfolio.findByPk(req.params.id, {
-        include: [{ model: Investment,
-        attributes: [
-            "id",
-            "price",
-            "quantity",
-            "portfolio_id",
-        ],
-        include: [{ model: Tickers, 
-        attributes: [
-            "symbol",
-            "name"
-        ]}]
-    },
-] 
-});
-
-if (!portfolioData) {
-    res.status(404).json({ message: 'No portfolio found with this id!' });
-    return;
-  }
-else {
-  res.status(200).json(portfolioData);
-}
-} catch (err) {
-  res.status(500).json(err);
-  console.log(err);
-}
-=======
         const portfolioData = await Portfolio.findByPk(req.params.id, {
             include: [{
                 model: Investment,
@@ -85,7 +51,6 @@ else {
         console.log(err)
         res.status(500).json(err);
     }
->>>>>>> 4599e36f390db606f1e297fb97c7df4159633f28
 });
 
 router.delete("/:id", async (req, res) => {
