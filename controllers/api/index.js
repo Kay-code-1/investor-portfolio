208435175399bb241914823f73b7331509d711f2
kalyani joshi <kalyani121@gmail.com>
2021-09-17 15:02:38 -0400

    views to save investment

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const userRoutes = require('./userRoutes');
 const portfolioRoutes = require('./portfolioRoutes');
 const investmentsRoutes = require('./investmentsRoutes');
 const tickerRoutes = require('./tickerRoutes');
+const polygonRoutes = require('./polygon');
 
 router.use('/users', userRoutes);
 router.use('/portfolio', portfolioRoutes);
 router.use('/investments', investmentsRoutes);
 router.use('/tickers', tickerRoutes);
+router.use('/polygon', polygonRoutes);
 
 module.exports = router;
--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -5,3 +5,32 @@ const addInvestmentHandler = () => {
 document
   .getElementById("add-investment")
   .addEventListener("click", addInvestmentHandler);
+
+window.addEventListener("DOMContentLoaded", async () => {
+
+  const invRows = document.getElementById("investment-list-rows");
+  let paths = window.location.pathname.split("/");
+  let portfolioId = paths[2];
+
+  const response = await fetch("/api/portfolio/" + portfolioId, {
+    method: 'GET'
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+
+    const investments = result.investments.map(inv => {
+      return `<tr>
+        <td>${inv.ticker.symbol}</td>
+        <td>${inv.ticker.name}</td>
+        <td>${inv.quantity}</td>
+        <td>${inv.price}</td>
+        <td>${inv.price * inv.quantity}</td>
+      </tr>`;
+    });
+
+    invRows.innerHTML = investments.join("");
+  }
+
+
+});
