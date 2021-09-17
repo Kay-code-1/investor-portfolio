4e42dcef8012b84b8ef87a66b6b07c15fa7a7b7e
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-09-17 13:56:05 -0400

    saving

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const userRoutes = require('./userRoutes');
 const portfolioRoutes = require('./portfolioRoutes');
 const investmentsRoutes = require('./investmentsRoutes');
 const tickerRoutes = require('./tickerRoutes');
+const marketDataRoutes = require('./marketDataRoutes');
 
 router.use('/users', userRoutes);
 router.use('/portfolio', portfolioRoutes);
 router.use('/investments', investmentsRoutes);
 router.use('/tickers', tickerRoutes);
+router.use('/marketdata', marketDataRoutes);
 
 module.exports = router;
